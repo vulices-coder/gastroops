95c748386634c2b5b24518e0f614941e38078c66
CholoCreator <v.ulices@gmail.com>
2026-05-15 14:45:14 +0200

    fix(frontend): use proper VITE_API_URL in axios

--- gastroops-frontend/src/api/client.ts
+++ gastroops-frontend/src/api/client.ts
@@ -1,13 +1,11 @@
-import axios from "axios"
+import axios from "axios";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  timeout: 5000,
-  withCredentials: false,
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
   },
-})
+});
 
-export default api
+export default api;
